9f70386918847c135075512575b4f69be8dc94e2
Temutjin2k <tamutdzhin2006@mail.ru>
2025-04-17 00:20:52 +0500

    Defined proto files

--- user_service/internal/adapter/grpc/server/interface.go
+++ user_service/internal/adapter/grpc/server/interface.go
@@ -1,7 +1,12 @@
 package server
 
+import (
+	"context"
+	"user_service/internal/model"
+)
+
 type UserUseCase interface {
-	Register()
-	Authenticate()
-	GetProfile()
+	Register(ctx context.Context, user model.User) (model.User, error)
+	Authenticate(ctx context.Context, user model.User) (model.Token, error)
+	GetProfile(ctx context.Context, id int64) (model.User, error)
 }
